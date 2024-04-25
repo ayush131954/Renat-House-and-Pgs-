e6fb3e39afd9af24eb3fa2558f436a25bd597d11
Ayush Patel <126762489+ayush131954@users.noreply.github.com>
2024-04-25 13:46:46 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,6 +0,0 @@
-const burgerBtn = document.querySelector(".menu-burger-btn");
-const burgerMenu = document.querySelector(".menu-burger");
-
-burgerBtn.addEventListener("click", () => {
-  burgerMenu.classList.toggle("d-none");
-});
